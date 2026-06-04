c04b0442180daeb39806da5f15f13f24e052dce2
Alan Altruy <alan.evy@gmail.com>
2026-06-04 02:52:50 +0200

    autonom theme switching

--- Image_Search_Engine_2026/app/static/js/theme.js
+++ Image_Search_Engine_2026/app/static/js/theme.js
@@ -1,2 +1,17 @@
-const theme = new URLSearchParams(window.location.search).get("theme");
-document.documentElement.setAttribute("data-theme", theme);
+const params = new URLSearchParams(window.location.search);
+let theme = params.get("theme");
+
+// 1. si Android impose un thème
+if (theme === "dark" || theme === "light") {
+    document.documentElement.setAttribute("data-theme", theme);
+}
+
+// 2. sinon AUTO
+else {
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    document.documentElement.setAttribute(
+        "data-theme",
+        prefersDark ? "dark" : "light"
+    );
+}
